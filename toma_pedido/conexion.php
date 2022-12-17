1d2dc0507ad57d1c747071fec1771e87fba9baf0
BraRodri <38147967+BraRodri@users.noreply.github.com>
2022-12-17 15:51:32 -0500

    Update conexion.php

--- toma_pedido/conexion.php
+++ toma_pedido/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-    $dbconn = pg_connect("host=192.168.1.12 dbname=toma_pedidos user=desarrolloaxa password=Ax4Desarr0ll0")
+    $dbconn = pg_connect("host=172.16.1.6 dbname=toma_pedidos user=desarrolloaxa password=Ax4Desarr0ll0")
     or die('No se ha podido conectar: ' . pg_last_error());
 
 	$dbconn_pruebas = pg_connect("host=192.168.1.48 dbname=toma_pedidos_prueba user=desarrolloaxa password=Ax4Desarr0ll0")
